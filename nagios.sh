3e686f554fda944bbe60ff2d64682e7ed7ce2fb0
Sfenner <55862990+Sfenner@users.noreply.github.com>
2020-04-16 20:34:46 -0700

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -28,7 +28,7 @@ mkdir /etc/nagios/servers
 sed -i 's,#cfg_dir=/etc/nagios/servers,cfg_dir=/etc/nagios/servers,g' /etc/nagios/nagios.cfg
 echo 'define command{
                           command_name check_nrpe
-                          command_line /usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESSS$ -c $ARG1$
+                          command_line /usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
                           }' >> /etc/nagios/objects/commands.cfg
                           
 systemctl restart nagios
